ec9a90053c5bb3696cf702f1da1ded9a1fe9f7ad
EngSteven <109984328+EngSteven@users.noreply.github.com>
2022-09-02 01:11:04 -0600

    main

--- progra.cpp
+++ progra.cpp
@@ -21,21 +21,22 @@ int main() {
     listA->insert(0, otroEvento2);
     listA->insert(0, otroEvento);
     listA->add(unEvento);
-
-    //listB->insert(0, new string("primer mensaje de prueba"));   //
-    //listB->insert(1, new string("segundo mensaje de prueba"));  
+    
+    listB->insert(0, new string("primer mensaje de prueba"));   //
+    listB->insert(1, new string("segundo mensaje de prueba"));  
     listB->add(new string("tercer mensaje de prueba"));        //
     listB->add(new string("cuarto mensaje de prueba"));        
     listB->add(new string("quinto mensaje de prueba"));         //
-
-    //listB->remove(0);
+    
+    listB->remove(0);
+    listB->remove(3);
     listB->remove(1);
-    //listB->remove(1);
 
     for(int i=0; i<listA->getSize(); i++) {
         Event *currentEvent = listA->find(i);
         cout << currentEvent->getTitle() << endl;
     }
+    
 
     cout << "==================================" << endl;
     for(int i=0; i<listB->getSize(); i++) {
